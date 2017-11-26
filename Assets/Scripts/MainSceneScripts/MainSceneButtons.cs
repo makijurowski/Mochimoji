6bf59e928f410df6fb77a7fa8800b0015b46c904
makiroggers <makiroggers@gmail.com>
2017-11-26 13:22:03 -0800

    Added audio on/off buttons

---
+++ Assets/Scripts/MainSceneScripts/MainSceneButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneButtons : MonoBehaviour
+{
+	public AudioClip buttonClickSound;
+	private AudioSource source;
+
+	public void loadStartSceneButton()
+	{
+		source = GetComponent<AudioSource>();
+		source.PlayOneShot(buttonClickSound);
+		SceneManager.LoadSceneAsync("GameStartScene", LoadSceneMode.Single);
+	}
+
+	public void endGameButton()
+	{
+		source = GetComponent<AudioSource>();
+		source.PlayOneShot(buttonClickSound);
+		SceneManager.LoadSceneAsync("GameOverScene", LoadSceneMode.Single);
+	}
+
+	public void audioButton()
+	{
+		source = GetComponent<AudioSource>();
+		source.PlayOneShot(buttonClickSound);
+	}
+
+	public void genericButton()
+	{
+		source = GetComponent<AudioSource>();
+		source.PlayOneShot(buttonClickSound);
+	}
+}
