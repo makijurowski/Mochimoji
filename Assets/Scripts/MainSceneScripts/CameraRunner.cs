adb8b7c6c9bc9c44d02ac1ecc011562dfbe91770
makiroggers <makiroggers@gmail.com>
2017-11-26 15:46:31 -0800

    Adjusted on-screen controls to main scene

--- Assets/Scripts/MainSceneScripts/CameraRunner.cs
+++ Assets/Scripts/MainSceneScripts/CameraRunner.cs
@@ -13,8 +13,8 @@ public class CameraRunner : MonoBehaviour {
     // Have the camera follow the Player.
 	void Update () {
         if (player != null) {
-            // transform.position = new Vector3(player.position.x + (float)8 + x_offset, (float)0.57 + y_offset, -10);
-            transform.position = new Vector3(player.position.x + (float)8, (float)0.57, -10);
+            transform.position = new Vector3(player.position.x + (float)8 + x_offset, (float)0.57 + y_offset, -10);
+            // transform.position = new Vector3(player.position.x + (float)8, (float)0.57, -10);
         }
 	}
 }
